d68dc899c6882502b3dbc3257bf0b25dfccdef1b
VadimKashtanov <59579094+VadimKashtanov@users.noreply.github.com>
2024-06-26 00:46:15 +0200

    Add files via upload

--- def/opti.cuh
+++ def/opti.cuh
@@ -2,9 +2,43 @@
 
 #include "mdl.cuh"
 
-#define L2_regularisation 0.01
-#define ADAM 0
-#define ADAM_HISTOIRE 2
+#define L2_regularisation 0//0.01
+
+#define SGD 0
+#define SGD_____HISTOIRE 0
+
+#define MOMENT 1
+#define MOMENT__HISTOIRE 1
+
+#define RMSPROP 2
+#define RMSPROP_HISTOIRE 1
+
+#define ADAM 3
+#define ADAM____HISTOIRE 2
+
+void sgd(
+	Mdl_t   *  mdl,
+	float *** hist,	//[hist][inst][p]
+	uint         i,
+	float    alpha,
+	uint         t
+);
+
+void moment(
+	Mdl_t   *  mdl,
+	float *** hist,	//[hist][inst][p]
+	uint         i,
+	float    alpha,
+	uint         t
+);
+
+void rmsprop(
+	Mdl_t   *  mdl,
+	float *** hist,	//[hist][inst][p]
+	uint         i,
+	float    alpha,
+	uint         t
+);
 
 void adam(
 	Mdl_t   *  mdl,
@@ -13,6 +47,8 @@ void adam(
 	float    alpha,
 	uint         t
 );
+
+//	---------------------------
 
 void opti(
 	Mdl_t     *     mdl,
